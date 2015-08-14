c371df8493a0f4ec15cb5972df9677223a5b9b17
Ariel Spear <simpleredfox@gmail.com>
2015-08-14 14:06:38 -0700

    test capital letters.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -14,4 +14,10 @@ public class AppTest {
     String result = "Th-s -s - s-nt-nc-.";
     assertEquals(result, App.removeVowels("This is a sentence."));
   }
+
+  @Test
+  public void removeVowels_worksWithCapitals_IlikeIsoptopeComics(){
+    String result = "- l-k- -s-t-p- C-m-cs.";
+    assertEquals(result, App.removeVowels("I like Isotope Comics."));
+  }
 }
